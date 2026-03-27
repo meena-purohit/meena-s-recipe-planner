a6b963382cd62d119bc44ea226724f34888a6637
meena-purohit <mpurohit7890@gmail.com>
2026-03-27 22:19:15 +0530

    fix: call .trim() method and clean up component exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import RecipeCard from "./components/RecipeCard";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  //Mock data to test our grid
+  const [searchTerm, setSearchTerm] = useState(""); //Track search input
+  
   const recipes = [
     {id: 1, title: "Spicy Pasta",image: "/pasta.jpg"},
     { id: 2, title: "Salad Bowl", image: "/salad.jpg" },
     { id: 3, title: "Grilled Chicken", image: "/chicken.jpg" },
     { id: 4, title: "Veggie Burger", image: "/burger.jpg" },
   ];
+  //Logic: Filter recipes based on the search term
+  const filteredRecipes = recipes.filter((recipe)=>
+    recipe.title.toLowerCase().includes(searchTerm.toLowerCase().trim())
+  );
   return(
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
         <h2 className="text-3xl font-bold text-gray-800 mb-8">Popular Recipes</h2>
+
+         <SearchBar  searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-           {recipes.map((recipe)=>(
+           {filteredRecipes.map((recipe)=>(
             <RecipeCard  key={recipe.id} recipe={recipe}/>
            ))}
         </div>
+
+        {filteredRecipes.length === 0 && (
+          <p className="text-center text-gray-500 mt-10">No recipes found. Try searching for something else</p>
+        )}
       </main>
     </div>
   );
